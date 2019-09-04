3f65106e995278088407665c7dd5308792b713c8
fildabert <filberttt@hotmail.com>
2019-09-05 02:45:21 +0700

    added pagination to GET /rides

--- src/app.ts
+++ src/app.ts
@@ -1,56 +1,68 @@
 'use strict';
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var jsonParser = bodyParser.json();
-module.exports = function (db) {
-    app.get('/health', function (req, res) { return res.send('Healthy'); });
-    app.post('/rides', jsonParser, function (req, res) {
-        var startLatitude = Number(req.body.start_lat);
-        var startLongitude = Number(req.body.start_long);
-        var endLatitude = Number(req.body.end_lat);
-        var endLongitude = Number(req.body.end_long);
-        var riderName = req.body.rider_name;
-        var driverName = req.body.driver_name;
-        var driverVehicle = req.body.driver_vehicle;
+
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+module.exports = (db) => {
+    app.get('/health', (req, res) => res.send('Healthy'));
+
+    app.post('/rides', jsonParser, (req, res) => {
+        const startLatitude: number = Number(req.body.start_lat);
+        const startLongitude: number = Number(req.body.start_long);
+        const endLatitude: number = Number(req.body.end_lat);
+        const endLongitude: number = Number(req.body.end_long);
+        const riderName: string = req.body.rider_name;
+        const driverName: string = req.body.driver_name;
+        const driverVehicle: string = req.body.driver_vehicle;
+        
         if (startLatitude < -90 || startLatitude > 90 || startLongitude < -180 || startLongitude > 180) {
             return res.send({
                 error_code: 'VALIDATION_ERROR',
                 message: 'Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively'
             });
         }
+
         if (endLatitude < -90 || endLatitude > 90 || endLongitude < -180 || endLongitude > 180) {
             return res.send({
                 error_code: 'VALIDATION_ERROR',
                 message: 'End latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively'
             });
         }
+
         if (typeof riderName !== 'string' || riderName.length < 1) {
             return res.send({
                 error_code: 'VALIDATION_ERROR',
                 message: 'Rider name must be a non empty string'
             });
         }
+
         if (typeof driverName !== 'string' || driverName.length < 1) {
             return res.send({
                 error_code: 'VALIDATION_ERROR',
                 message: 'Rider name must be a non empty string'
             });
         }
+
         if (typeof driverVehicle !== 'string' || driverVehicle.length < 1) {
             return res.send({
                 error_code: 'VALIDATION_ERROR',
                 message: 'Rider name must be a non empty string'
             });
         }
+
         var values = [req.body.start_lat, req.body.start_long, req.body.end_lat, req.body.end_long, req.body.rider_name, req.body.driver_name, req.body.driver_vehicle];
-        var result = db.run('INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)', values, function (err) {
+        
+        const result = db.run('INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)', values, function (err) {
             if (err) {
                 return res.send({
                     error_code: 'SERVER_ERROR',
                     message: 'Unknown error'
                 });
             }
+
             db.all('SELECT * FROM Rides WHERE rideID = ?', this.lastID, function (err, rows) {
                 if (err) {
                     return res.send({
@@ -58,11 +70,13 @@ module.exports = function (db) {
                         message: 'Unknown error'
                     });
                 }
+
                 res.send(rows);
             });
         });
     });
-    app.get('/rides', function (req, res) {
+
+    app.get('/rides', (req, res) => {
         db.all('SELECT * FROM Rides', function (err, rows) {
             if (err) {
                 return res.send({
@@ -70,43 +84,49 @@ module.exports = function (db) {
                     message: 'Unknown error'
                 });
             }
+
             if (rows.length === 0) {
                 return res.send({
                     error_code: 'RIDES_NOT_FOUND_ERROR',
                     message: 'Could not find any rides'
                 });
             }
-            var totalPages = Math.ceil(rows.length / 10);
-            var page = Number(req.query.page);
-            if (!page) {
+            const totalPages: number = Math.ceil(rows.length / 10);
+            var page: number = Number(req.query.page);
+            if(!page) {
                 page = 1;
             }
-            if (page > totalPages) {
-                page = totalPages;
+            if(page > totalPages) {
+                page = totalPages
             }
+
             res.send({
-                page: page,
-                totalPages: totalPages,
-                rides: rows.slice(page * 10 - 10, page * 10)
+                page,
+                totalPages,
+                rides: rows.slice(page * 10 -10, page * 10)
             });
         });
     });
-    app.get('/rides/:id', function (req, res) {
-        db.all("SELECT * FROM Rides WHERE rideID='" + req.params.id + "'", function (err, rows) {
+
+    app.get('/rides/:id', (req, res) => {
+        db.all(`SELECT * FROM Rides WHERE rideID='${req.params.id}'`, function (err, rows) {
             if (err) {
                 return res.send({
                     error_code: 'SERVER_ERROR',
                     message: 'Unknown error'
                 });
             }
+
             if (rows.length === 0) {
                 return res.send({
                     error_code: 'RIDES_NOT_FOUND_ERROR',
                     message: 'Could not find any rides'
                 });
             }
+
             res.send(rows);
         });
     });
+
     return app;
 };
